94bd5ce75b80be0a1665f8b9d2bd0c64bdeaa729
Kukushka549 <sviridovalexey073@gmail.com>
2025-06-22 03:02:11 +0300

    v0.01

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
+    Employee first = new Employee("Иван", "Иванов", "Иванович", 1, 10000);
+
+    System.out.println(first.getId());
+
+    Employee second = new Employee("Ива", "Ивано", "Иванови", 2, 20000);
+
+    System.out.println(second.getId());
+
+        System.out.println("first.equals(second) = " + first.equals(second));
+
+        System.out.println("first.hashCode(second) = " + second.hashCode());
 
     }
 }
